6c08a5d366bcb37933ed373f2ad72530ba0ba08a
Wendel Mendes <wendel.omendes@gmail.com>
2022-08-24 15:15:34 -0300

    x

---
+++ assets/scripts/script.js
@@ -0,0 +1,10 @@
+var scroll2 = window.document.getElementById('scroll');
+var linkedin2 = window.document.getElementById('linkedin')
+//linkedin2.addEventListener('mouseover', clicar)
+function clicar() {
+    scroll2.style.display = "none";
+    div.style.display = "block";
+}
+clicar()
+
+     
