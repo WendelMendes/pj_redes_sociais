3972cf91540b9648d73f5b16ef88d4bcd56e7cd8
Wendel Mendes <wendel.omendes@gmail.com>
2022-08-25 10:20:49 -0300

    :D

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,10 +1,12 @@
 var scroll2 = window.document.getElementById('scroll');
 var linkedin2 = window.document.getElementById('linkedin')
 //linkedin2.addEventListener('mouseover', clicar)
-function clicar() {
+function clicar2() {
     scroll2.style.display = "none";
     div.style.display = "block";
 }
-clicar()
+function clicar() {
+    scroll2.style.display = "initial";
+}
 
      
